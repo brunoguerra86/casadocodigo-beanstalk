efd46f8e41b7126a505145a897e5815606a5473a
brunoguerra <brunoguerra@hotmail.com>
2023-04-15 16:07:48 -0300

    build: ajustes nas dependências pom.xml

--- src/main/java/br/com/casadocodigo/loja/configuration/JPAConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/configuration/JPAConfiguration.java
@@ -57,7 +57,7 @@ public class JPAConfiguration {
 	public DataSource dataSourceDev() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setUsername("root");
-		dataSource.setPassword("");
+		dataSource.setPassword("123");
 		dataSource.setUrl("jdbc:mysql://localhost:3306/casadocodigo");
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 		return dataSource;
