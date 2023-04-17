2fd028acba59ef2aa4254eeaf695f1a2b69bfe8c
brunoguerra <brunoguerra@hotmail.com>
2023-04-17 08:11:28 -0300

    build: configuração Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 FROM tomcat:9-jre8
+
 LABEL authors="bg8"
+
 RUN rm -r /usr/local/tomcat/webapps/
+
 COPY ROOT.WAR /usr/local/tomcat/webapps/
--- src/main/java/br/com/casadocodigo/loja/configuration/JPAConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/configuration/JPAConfiguration.java
@@ -51,7 +51,8 @@ public class JPAConfiguration {
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 		return dataSource;
 	}
-	
+
+
 	@Bean
 	@Profile("dev")
 	public DataSource dataSourceDev() {
